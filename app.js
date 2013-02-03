23520d8b6d15bba5b1a2f51a213903d164f7bd2c
changhyun1.lee <changhyun1.lee@samsung.com>
2013-02-03 17:39:31 +0900

    move to public

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var http = require('http'),
           console.log('[Server] URL : ' + req.url);
 
           if (reqFile === '/' || reqFile === '/index.html') {
-              fs.readFile(__dirname + '/controller/index.html', function (err, data) { // controller ui of index.html
+              fs.readFile(__dirname + '/public/index.html', function (err, data) { // controller ui of index.html
                   if (err) {
                     res.writeHead(500);
                     return res.end('Error loading index.html');
@@ -15,8 +15,28 @@ var http = require('http'),
                   res.end(data);
                   console.log('[Server] Send index.html');
               });
-          } else if(reqFile === '/main.js') {
-              fs.readFile(__dirname + '/controller/main.js', function (err, data) {
+          } else if(reqFile === '/css/style.css') {
+              fs.readFile(__dirname + '/public/css/style.css', function (err, data) {
+                  if (err) {
+                      res.writeHead(500);
+                      return res.end('Error loading style.css');
+                  }
+                  res.writeHead(200, {'Content-Type': 'text/css'});
+                  res.end(data);
+                  console.log('[Server] Send  style.css');
+              });
+          } else if(reqFile === '/js/jquery-1.7.1.min.js') {
+              fs.readFile(__dirname + '/public/js/jquery-1.7.1.min.js', function (err, data) {
+                  if (err) {
+                      res.writeHead(500);
+                      return res.end('Error loading jquery-1.7.1.min.js');
+                  }
+                  res.writeHead(200, {'Content-Type': 'application/javascript'});
+                  res.end(data);
+                  console.log('[Server] Send  jquery-1.7.1.min.js');
+              });
+          } else if(reqFile === '/js/main.js') {
+              fs.readFile(__dirname + '/public/js/main.js', function (err, data) {
                   if (err) {
                       res.writeHead(500);
                       return res.end('Error loading main.js');
@@ -24,16 +44,6 @@ var http = require('http'),
                   res.writeHead(200, {'Content-Type': 'application/javascript'});
                   res.end(data);
                   console.log('[Server] Send  main.js');
-              });
-          } else if (reqFile === '/webslidecontrol.js') {
-              fs.readFile(__dirname + '/webslidecontrol.js', function (err, data) {
-                  if (err) {
-                      res.writeHead(500);
-                      return res.end('Error loading main.js');
-                  }
-                  res.writeHead(200, {'Content-Type': 'application/javascript'});
-                  res.end(data);
-                  console.log('[Server] Send webslidecontrol.js');
               });
           } else if (reqFile === '/favicon.ico') {
               fs.readFile(__dirname + '/favicon.ico', function (err, data) {
@@ -44,6 +54,16 @@ var http = require('http'),
                   res.writeHead(200, {'Content-Type': 'image/x-icon'});
                   res.end(data);
                   console.log('[Server] Send favicon.ico');
+              });
+          } else if (reqFile === '/webslidecontrol.js') {
+              fs.readFile(__dirname + '/webslidecontrol.js', function (err, data) {
+                  if (err) {
+                      res.writeHead(500);
+                      return res.end('Error loading main.js');
+                  }
+                  res.writeHead(200, {'Content-Type': 'application/javascript'});
+                  res.end(data);
+                  console.log('[Server] Send webslidecontrol.js');
               });
           }
       });
