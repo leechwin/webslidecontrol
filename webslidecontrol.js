cc02258d31084f54b0e6a661fd993363200d935a
changhyun1.lee <changhyun1.lee@samsung.com>
2013-02-07 23:55:24 +0900

    Added slide-presentation.js

--- webslidecontrol.js
+++ webslidecontrol.js
@@ -26,6 +26,9 @@ var init = function () {
 };
 $(document).ready(init);
 
+
+var slidePresentation;
+
 /**
  * presentation starts
  */
@@ -34,6 +37,11 @@ function start() {
         impress().goto( 0 );
     } else if (typeof Reveal !== 'undefined') {
         Reveal.slide( 0 );
+    } else if (typeof SlideManager !== 'undefined') {
+        if (typeof slidePresentation === 'undefined') {
+            slidePresentation = new SlideManager();
+        }
+        slidePresentation.toggleSpeakerNotes();
     }
 }
 
@@ -42,49 +50,59 @@ function start() {
  */
 function end() {
     if (typeof impress !== 'undefined') {
-        console.log("count: " + $('.step').length);
         impress().goto($('.step').length - 1);
     } else if (typeof Reveal !== 'undefined') {
         Reveal.slide( Number.MAX_VALUE );
-        
+    } else if (typeof SlideManager !== 'undefined') {
+        if (typeof slidePresentation === 'undefined') {
+            slidePresentation = new SlideManager();
+        }
+        slidePresentation.highlightImportantCode();
     }
 }
 
+/**
+ * presentation pre
+ */
 function pre() {
     if (typeof impress !== 'undefined') {
         impress().prev();
     } else if (typeof Reveal !== 'undefined') {
         Reveal.navigatePrev();
+    } else if (typeof SlideManager !== 'undefined') {
+        if (typeof slidePresentation === 'undefined') {
+            slidePresentation = new SlideManager();
+        }
+        slidePresentation.prev();
     }
-
-    //impress().prev();
-    //Reveal.navigatePrev();
 }
 
+/**
+ * presentation next
+ */
 function next() {
     if (typeof impress !== 'undefined') {
         impress().next();
     } else if (typeof Reveal !== 'undefined') {
         Reveal.navigateNext();
+    } else if (typeof SlideManager !== 'undefined') {
+        if (typeof slidePresentation === 'undefined') {
+            slidePresentation = new SlideManager();
+        }
+        slidePresentation.next();
     }
-    // clickevent();
-    //$(document).trigger($.Event("keydown", { keyCode: 38 }))
-
-
-    // impress().next();
-    // impress().goto(10);
-    //Reveal.navigateNext();
 }
 
-function clickevent()
-{                
+/* TODO::
+function clickevent(){                
      var e = $.Event("keydown");
      e.which = 38;
      e.keyCode = 38;
      $(document).trigger(e);     
 }
 
-/* TODO::
+$(document).trigger($.Event("keydown", { keyCode: 38 }))
+
 function simulateKeyPress(character) {
     var e = $.Event("keydown");
     e.which = 39; // # Some key code value
